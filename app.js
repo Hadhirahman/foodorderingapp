2cbd09df706bd37b114e5bdb8b6641a4f2ba6ac5
Hadhirahman <hadhirahmanmt@gmail.com>
2025-02-16 15:40:47 +0530

    studie body parcel

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const parent = React.createElement('div', { id: "parent" }, [
+    React.createElement('div', {}, [
+        React.createElement("h1", {}, "this is h1 for child 1"),
+        React.createElement("h2", {}, "this is h2 for child 1")]),
+    React.createElement('div', {}, [
+        React.createElement("h1", {}, "this is h1 for child2 "),
+        React.createElement("h2", {}, "this is h2 for child 2")])]);
 
 
 
-const parent=React.createElement('div',{id:"parent"},[React.createElement('div',{},[React.createElement("h1",{},"this is h1 for child 1"),React.createElement("h2",{},"this is h2 for child 1")]),React.createElement('div',{},[React.createElement("h1",{},"this is h1 for child2 "),React.createElement("h2",{},"this is h2 for child 2")])]);
-
-
-
-const root= ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(parent)
