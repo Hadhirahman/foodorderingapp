fddfc8830215d0b245ffe961666110a661a446ac
Hadhirahman <hadhirahmanmt@gmail.com>
2025-02-18 22:48:26 +0530

    use state covred

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,25 @@
 import RestoCards from "./RestoCards"
 import { restaurants } from "./utils/MockData"
+import { useState } from "react"
 
 
 const Body=() => {
+
+const [restaurent, setRestaurent] = useState(restaurants)
+
+
     return (
         <div className="body-container">
     <h2 className="title">Top Restaurants</h2>
+    <div className="btn-container">
+        <button className="btn" onClick={()=>{
+            const filterdata =restaurent.filter((restaurentfilter)=>restaurentfilter.rating>4)
+            setRestaurent(filterdata)
+        }}>top rating restaurant</button>
+    </div>
     <div className="card-container">
-    {restaurants.map((restaurantlist)=>{
-     return <RestoCards resdata={restaurantlist} />  
+    {restaurent.map((restaurantlist)=>{
+     return <RestoCards key={restaurantlist.id} resdata={restaurantlist} />  
 
     })}
     </div>
--- src/components/utils/mockData.jsx
+++ src/components/utils/mockData.jsx
@@ -12,7 +12,7 @@ export const restaurants = [
       name: "Sushi Paradise", 
       image: "https://img.freepik.com/free-photo/lavash-rolls-with-stuffings-tomatoes_114579-3418.jpg?t=st=1739818901~exp=1739822501~hmac=b2d960d380314c98d022119b0af749345a8c11defd219b82144de3c63f8c4d46&w=1060", 
       price: "$15", 
-      rating: 4.7, 
+      rating: 4.1, 
       cuisine: "Japanese" 
     },
     { 
@@ -20,7 +20,7 @@ export const restaurants = [
       name: "BBQ Heaven", 
       image: "https://img.freepik.com/free-photo/fried-vegetables-with-sauce-pan_1220-5230.jpg?t=st=1739818612~exp=1739822212~hmac=d9f631712ce9082c76e2d72b5f1ffd767b420e20743985994a478911f6db8706&w=1380", 
       price: "$20", 
-      rating: 4.3, 
+      rating: 3.3, 
       cuisine: "BBQ" 
     },
     { 
@@ -28,34 +28,34 @@ export const restaurants = [
       name: "Vegan Delight", 
       image: "https://img.freepik.com/free-photo/lavash-rolls-with-stuffings-tomatoes_114579-3418.jpg?t=st=1739818901~exp=1739822501~hmac=b2d960d380314c98d022119b0af749345a8c11defd219b82144de3c63f8c4d46&w=1060", 
       price: "$12", 
-      rating: 4.8, 
+      rating: 3.8, 
       cuisine: "Vegan" 
     }, { 
-        id: 1, 
+        id: 5, 
         name: "Italian Bistro", 
         image: "https://img.freepik.com/free-photo/fried-vegetables-with-sauce-pan_1220-5230.jpg?t=st=1739818612~exp=1739822212~hmac=d9f631712ce9082c76e2d72b5f1ffd767b420e20743985994a478911f6db8706&w=1380", 
         price: "$15", 
-        rating: 4.5, 
+        rating: 4.4, 
         cuisine: "Italian" 
       },
       { 
-        id: 2, 
+        id: 6, 
         name: "Sushi Paradise", 
         image: "https://img.freepik.com/free-photo/lavash-rolls-with-stuffings-tomatoes_114579-3418.jpg?t=st=1739818901~exp=1739822501~hmac=b2d960d380314c98d022119b0af749345a8c11defd219b82144de3c63f8c4d46&w=1060", 
         price: "$18", 
-        rating: 4.7, 
+        rating: 4.2, 
         cuisine: "Japanese" 
       },
       { 
-        id: 3, 
+        id: 7, 
         name: "BBQ Heaven", 
         image: "https://img.freepik.com/free-photo/fried-vegetables-with-sauce-pan_1220-5230.jpg?t=st=1739818612~exp=1739822212~hmac=d9f631712ce9082c76e2d72b5f1ffd767b420e20743985994a478911f6db8706&w=1380", 
         price: "$21", 
-        rating: 4.3, 
+        rating: 3.9, 
         cuisine: "BBQ" 
       },
       { 
-        id: 4, 
+        id: 8, 
         name: "Vegan Delight", 
         image: "https://img.freepik.com/free-photo/lavash-rolls-with-stuffings-tomatoes_114579-3418.jpg?t=st=1739818901~exp=1739822501~hmac=b2d960d380314c98d022119b0af749345a8c11defd219b82144de3c63f8c4d46&w=1060", 
         price: "$9", 
