01001bca9b7efb276598fe5003c7456bde5b903f
Hadhirahman <hadhirahmanmt@gmail.com>
2025-02-20 13:23:33 +0530

    filter search

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,6 +6,11 @@ import Shimmer from "./Shimmer"
 const Body=() => {
 
 const [restaurent, setRestaurent] = useState([])
+const [filteredrestaurent,setFilteredrestsurent]=useState([])
+
+const [searchtext, setSearchtext] = useState("")
+
+
 
 
 useEffect(()=>{
@@ -16,32 +21,31 @@ const fetchdata=async()=>{
     const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=10.51600&lng=76.21570&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
     const jsondata=await data.json()
     // console.log(jsondata.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
-    setRestaurent(jsondata.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+    setRestaurent(jsondata?.data?.cards[1]?.card.card?.gridElements?.infoWithStyle?.restaurants)
+    setFilteredrestsurent(jsondata?.data?.cards[1]?.card.card?.gridElements?.infoWithStyle?.restaurants)
+
     
 }
 
-if(restaurent.length===0){
 
-    return (
+
+
+    return restaurent.length===0 ?(
         <div className="body-container">
 
-       <div className="card-container">
-      <Shimmer/>
-      <Shimmer/>
-      <Shimmer/>
-      <Shimmer/>
-      <Shimmer/>
-      <Shimmer/>
-      <Shimmer/>
-      <Shimmer/>
-      <Shimmer/>
-       </div>
-       </div>
-    )
-}
-
-
-    return (
+        <div className="card-container">
+       <Shimmer/>
+       <Shimmer/>
+       <Shimmer/>
+       <Shimmer/>
+       <Shimmer/>
+       <Shimmer/>
+       <Shimmer/>
+       <Shimmer/>
+       <Shimmer/>
+        </div>
+        </div>
+    ):(
         <div className="body-container">
     <h2 className="title">Top Restaurants</h2>
     <div className="btn-container">
@@ -49,9 +53,28 @@ if(restaurent.length===0){
             const filterdata =restaurent.filter((restaurentfilter)=>restaurentfilter.info.avgRatingString>4.5)
             setRestaurent(filterdata)
         }}>top rating restaurant</button>
+<div className="search_container">
+
+
+<input type="text" placeholder="search" className="search_input" value={searchtext} onChange={(e)=>{setSearchtext(e.target.value)}}/>
+
+
+<button className="search_btn"  onClick={()=>{
+    const filterRestaurent=restaurent.filter((searchfilter)=>
+        
+     searchfilter.info.name.toLowerCase().includes(searchtext.toLowerCase())
+     
+    )
+    setFilteredrestsurent(filterRestaurent);
+    
+    
+
+}}>search</button> 
+</div>
+
     </div>
     <div className="card-container">
-    {restaurent.map((restaurantlist)=>{
+    {filteredrestaurent.map((restaurantlist)=>{
         {/* console.log(restaurantlist); */}
      return <RestoCards key={restaurantlist.info.id} resdata={restaurantlist.info} />  
 
--- src/components/Heder.jsx
+++ src/components/Heder.jsx
@@ -1,5 +1,10 @@
+import React, { useState } from 'react'
 const Header = () => {
+    const [loginbtn,setLoginbtn]=useState("login")
+
+
     return (
+
         <header className="header">
         <div className="container">
           {/* Logo */}
@@ -13,6 +18,9 @@ const Header = () => {
             <a href="#">About</a>
             <a href="#">Services</a>
             <a href="#">Contact</a>
+            <button className='loginbtn' onClick={()=>{
+               loginbtn ==="login"? setLoginbtn("Logout"):setLoginbtn("login")
+            }}>{loginbtn}</button>
           </nav>
   
         </div>
